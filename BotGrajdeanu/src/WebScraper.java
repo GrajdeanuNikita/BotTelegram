3f1410537a5979764036bd986859468fb4d368e6
GRAJDEANU NIKITA <9698080@itisrossi.edu.it>
2024-12-20 11:04:13 +0100

    inzio interezioni

--- BotGrajdeanu/src/WebScraper.java
+++ BotGrajdeanu/src/WebScraper.java
@@ -1,2 +1,55 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import java.util.List;
+
+
 public class WebScraper {
+
+    private WebDriver driver;
+
+    public WebScraper(WebDriver driver){
+        this.driver= driver;
+    }
+
+    public void Trovafilm(String genere, int pagine) throws InterruptedException{
+        String url = "https://www.imdb.com/search/title/?genres=" + genere;
+
+        driver.get(url);
+        System.out.println("\nFetching movies for genre: " + genere + "\n");
+
+        int pageCount = 1;
+
+        while (pageCount <= pagine) {
+            System.out.println("Page " + pageCount + ":");
+
+            Thread.sleep(3000);
+
+
+            List<WebElement> movieElements = driver.findElements(By.cssSelector(".ipc-title__text"));
+            System.out.println("Found " + movieElements.size() + " movie(s).");
+
+
+            if (movieElements.isEmpty()) {
+                System.out.println("No movies found on this page.");
+                break;
+            }
+
+            for (WebElement movie : movieElements) {
+                System.out.println("Found movie: " + movie.getText());
+            }
+
+
+            List<WebElement> nextButton = driver.findElements(By.cssSelector(".lister-page-next"));
+            System.out.println("Found next button: " + nextButton.size());
+            if (!nextButton.isEmpty()) {
+                nextButton.get(0).click();
+                pageCount++;
+            } else {
+                System.out.println("No more pages found.");
+                break;
+            }
+        }
+    }
 }
+
